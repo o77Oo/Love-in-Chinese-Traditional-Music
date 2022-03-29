4786954028ed3a249353217a6fa8a769d4752c5d
o77Oo <kaishibenben@gmail.com>
2022-03-28 23:29:02 -0600

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,54 @@
+import "./Header.scss";
+import { Link } from "react-router-dom";
+import logo from "../../assets/logo/logo.jpeg";
+
+function Header() {
+    return (
+  
+        <header className="header">
+            <div className="header__container">
+        <div className="header__box">
+          <Link to="/">
+            <img className="header__logo" src={logo} alt="main" />
+          </Link>
+        </div>
+
+        <ul className="header__nav">
+          <li className="header__item">
+            <NavLink
+              className="header__link"
+              activeClassName="header__link--active"
+              to="/education"
+              exact
+            >
+             Education
+            </NavLink>
+          </li>
+          <li className="header__item">
+            <NavLink
+              className="header__link"
+              activeClassName="header__link--active"
+              to="/orchestra"
+            >
+              Orchestra
+            </NavLink>
+          </li>
+          <li className="header__item">
+            <NavLink
+              className="header__link"
+              activeClassName="header__link--active"
+              to="/"
+            >
+              Music
+            </NavLink>
+          </li>
+          
+        </ul>
+      </div>
+        </header>
+      
+    );
+  }
+  
+  export default Header;
+  
---
+++ src/page/Music/Music.js
@@ -0,0 +1,7 @@
+import { Component } from "react";
+import axios from "axios";
+import "../App.scss";
+import React from "react";
+import Header from "../components/Header/Header";
+import Comments from "../components/Comments/Comments";
+import SideBar from "../components/SideBar/SideBar";
